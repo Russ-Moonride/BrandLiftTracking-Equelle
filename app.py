52698992134aee1e3c805080b85ca54b35278de5
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2024-02-14 12:22:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,55 @@
+import streamlit as st
 import pandas as pd
+import pandas_gbq
+import pandas 
+import os
+from google.oauth2 import service_account
+from google.cloud import bigquery
+from datetime import datetime, timedelta
+from scipy.stats import chi2_contingency
+from PIL import Image
+from git import Repo
+import base64
+import requests
+import json
+from google.cloud import storage
+
+credentials = service_account.Credentials.from_service_account_info(
+          st.secrets["gcp_service_account"]
+      )
+client = bigquery.Client(credentials=credentials)
+
+Account = "Equelle"
+correct_hashed_password = "CFEmbodiedAdTest4305$"
+
+def initialize_storage_client():
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+    storage_client = storage.Client(credentials=credentials)
+    return storage_client
+
+# Use this client for GCS operations
+storage_client = initialize_storage_client()
+
+def password_protection():
+  if 'authenticated' not in st.session_state:
+      st.session_state.authenticated = False
+      
+  if not st.session_state.authenticated:
+      password = st.text_input("Enter Password:", type="password")
+      
+      if st.button("Login"):
+          if password == correct_hashed_password:
+              st.session_state.authenticated = True
+              main_dashboard()
+          else:
+              st.error("Incorrect Password. Please try again or contact the administrator.")
+  else:
+      main_dashboard()
+
+def main_dashboard():
+  st.markdown(f"<h1 style='text-align: center;'>{Account} Brand Lift Tracking</h1>", unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    password_protection()
