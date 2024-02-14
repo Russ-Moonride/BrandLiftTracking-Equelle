6e08ab9cf26b0b32fb7538ffbe80f0913178fdbd
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2024-02-14 14:50:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -96,8 +96,10 @@ def main_dashboard():
 
   #Format agg_data1 correctly
   agg_data1['Impressions'] = agg_data1['Impressions'].map(lambda x: "{:,}".format(int(x))) 
+  agg_data1['Clicks'] = agg_data1['Impressions'].map(lambda x: "{:,}".format(int(x)))   
+  agg_data1['Cost'] = agg_data1['Impressions'].map(lambda x: "{:,}".format(int(x)))
           
-  agg_data1['Cost'] = round(agg_data1['Cost'], 0).astype(int)
+  #agg_data1['Cost'] = round(agg_data1['Cost'], 0).astype(int)
   agg_data1['Cost'] = agg_data1['Cost'].apply(lambda x: f"${x}")
           
   agg_data1['CPC'] = round(agg_data1['CPC'], 0).astype(int)
