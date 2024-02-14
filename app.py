3cb3e34e933878decbde79da5cbdfc71b0b8d108
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2024-02-14 13:14:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,9 @@ def main_dashboard():
 
   # Filtering the dataset for the selected date ranges
   filtered_df1 = full_data[(full_data['Date'] >= start_date_1) & (full_data['Date'] <= end_date_1)]
-  agg_data1 = filtered_df1.select_dtypes(include='number').sum().to_frame('Sum Period 1')
+  agg_data1 = filtered_df1.select_dtypes(include='number').sum().to_frame('Sum Period 1').T
+
+  st.write(agg_data.columns) 
           
   #agg_data1['CPC'] = agg_data1['Cost']/agg_data1['Clicks']
   #agg_data1['CPM'] = (agg_data1['Cost']/agg_data1['Impressions'])*1000
