0091c11f2b617924ec5659b621fa0ac934eb8c18
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2024-02-15 15:07:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ client = bigquery.Client(credentials=credentials)
 Account = "Equelle"
 correct_hashed_password = "CFEquelleBL1940$"
 
-st.set_page_config(page_title= f"{Account} Creative Ad Testing Dash",page_icon="🧑‍🚀")
+st.set_page_config(page_title= f"{Account} Creative Ad Testing Dash",page_icon="🧑‍🚀",layout = 'wide')
 
 def initialize_storage_client():
     credentials = service_account.Credentials.from_service_account_info(
