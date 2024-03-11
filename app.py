12825014fb5aa8f02acdde467aa0645f07dce214
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2024-03-11 09:17:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def main_dashboard():
           
   sum_df1 = sum_df1.reset_index(drop=True)
   sum_df2 = sum_df2.reset_index(drop=True)
-  percentage_diff = ((sum_df1 - sum_df2) / sum_df1) * 100
+  percentage_diff = (((sum_df1 - sum_df2) / sum_df1) * 100) *-1
           
   #Format agg_data1 correctly
   agg_data1['Impressions'] = agg_data1['Impressions'].map(lambda x: "{:,}".format(int(x))) 
